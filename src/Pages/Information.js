3a95dac2f3bc7678c7167faf93bd406d21246294
VLa24 <123402491+SVA44@users.noreply.github.com>
2023-12-21 18:18:08 +0700

    Add debugging tools and adjust CSS styling

--- src/Pages/Information.js
+++ src/Pages/Information.js
@@ -1,4 +1,3 @@
-import '../Components/Contents/Grid.js';
 import ResponsiveGrid from '../Components/Contents/Grid.js';
 import Header from '../Components/UI/Header.js';
 import Footer from '../Components/UI/Footer.js';
@@ -7,8 +6,8 @@ import Footer from '../Components/UI/Footer.js';
 const Information = ({url}) => {
     return (
         <div>
-            <Header/>
-            <ResponsiveGrid/>
+            <Header />
+            <ResponsiveGrid url={url}/>
             <Footer/>
         </div> 
     );
